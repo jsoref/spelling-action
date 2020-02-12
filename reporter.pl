446deeb55ca8b3d51033793c65ecfbce922a5735
Josh Soref <jsoref@users.noreply.github.com>
2020-02-12 01:42:20 -0500

    Add matcher reporting

---
+++ reporter.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/env perl
+
+die 'Please set $tokens' unless defined $ENV{tokens};
+my $tokens=$ENV{tokens};
+exit 0 unless $tokens =~ /\w/;
+$tokens=~ s/\s+/|/g;
+my $re = "\\b($tokens)\\b";
+
+my $previous='';
+my $first_line=0;
+while (<>) {
+  if ($previous ne $ARGV) {
+    $previous=$ARGV;
+    $first_line = $. - 1;
+  }
+  next unless $_ =~ /$re/;
+  while (/$re/g) {
+    my ($start, $token) = (1 + length $`, $1);
+    my $line = $. - $first_line;
+    my $stop = $start + (length $token) - 1;
+    print "$ARGV: line $line, columns $start-$stop, Warning - '$token' is not a recognized word. (unrecognized-spelling)\n";
+  }
+}
